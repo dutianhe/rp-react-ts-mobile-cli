ba2e8a5963746fe60f251451e1804f5eff4b692d
tianhe.du <dutianhe@ruubypay.com>
2024-02-26 10:52:56 +0800

    解决部署路由问题

--- src/RootComponent.tsx
+++ src/RootComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import NotFoundPage from './pages/NotFoundPage'
 import './styles/main.sass'
 import router from './routes'
